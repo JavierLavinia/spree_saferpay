a4cb6bf70616220217676833e0cf2703e154e497
Ruben Sierra <maguilag@gmail.com>
2015-04-13 13:38:48 +0200

    Success link is not payment state

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -103,7 +103,7 @@ module Spree
 
     def order_url_options order, payment_method_id
       {
-        successlink: saferpay_confirm_order_checkout_url(order, state: :payment),
+        successlink: saferpay_confirm_order_checkout_url(order),
         faillink: edit_order_checkout_url(order, state: :payment),
         backlink: edit_order_checkout_url(order, state: :payment),
         notifyurl: saferpay_notify_order_checkout_url(order, payment_method_id: payment_method_id)
